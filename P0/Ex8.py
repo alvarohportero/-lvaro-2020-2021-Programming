a0ef263ab39b6bf534a69e9ec662bc0b3e6b132c
Alvaro <a.herrador.2020@alumnos.urjc.es>
2021-02-28 13:36:13 +0100

    P0 exercise

--- P0/Ex8.py
+++ P0/Ex8.py
@@ -2,13 +2,16 @@ import seq_0
 
 GENE_FOLDER = "./sequences/"
 
-gene_list= ["U5", "ADA", "FRAT1", "FXN"]
+gene_list = ["U5", "ADA", "FRAT1", "FXN"]
 base_list = ["A", "C", "T", "G"]
 
 print("-----| Exercise 8 |------")
 for gene in gene_list:
     sequence = seq_0.seq_read_fasta(GENE_FOLDER + gene + ".txt")
     print("GENE:", gene)
+    retro = 0
     for base in base_list:
-            chain = seq_0.seq_count_base(sequence,base)
-    print("Most frequent base:")
+        chain = seq_0.seq_count_base(sequence, base)
+        if chain >= retro:
+            retro = chain
+    print("Gene:",gene,  "Most frequent base:",base )
